3fbd8cc74a97fd03fcaf027a7a10f19e128427b2
alessanderleite <alessander.logistica@gmail.com>
2019-07-22 23:35:59 -0300

    add the required annotations to the User model class

--- src/main/java/br/com/alessanderleite/app/model/User.java
+++ src/main/java/br/com/alessanderleite/app/model/User.java
@@ -2,13 +2,34 @@ package br.com.alessanderleite.app.model;
 
 import java.util.Collection;
 
-public class User {
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(
+		name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
 	private Collection<Role> roles;
 	
 	public User() {}
