c6594e6169ddc645389bb9427ea973a43dae9555
alessanderleite <alessander.logistica@gmail.com>
2019-07-26 22:37:41 -0300

    make the UserService interface extends UserDetailsServices interface

--- src/main/java/br/com/alessanderleite/app/service/UserService.java
+++ src/main/java/br/com/alessanderleite/app/service/UserService.java
@@ -1,5 +1,12 @@
 package br.com.alessanderleite.app.service;
 
-public interface UserService {
+import org.springframework.security.core.userdetails.UserDetailsService;
 
+import br.com.alessanderleite.app.model.User;
+import br.com.alessanderleite.app.web.dto.UserRegistrationDto;
+
+public interface UserService extends UserDetailsService{
+
+	User findByEmail(String email);
+	User save(UserRegistrationDto registration);
 }
